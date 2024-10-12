3f4dd0f058c3d2a585902069df8958ed2f2a0908
Joshua Chadwick <jchadwick423@gmail.com>
2024-10-12 01:23:51 -0700

    Limited the number of Amazon products that could be read into the json file to obey the REST API transfer limits set by Google.

--- createData.py
+++ createData.py
@@ -3,27 +3,28 @@ import json
 filename = "./amazon-meta.txt"
 out = open("amazon.json", "w", encoding="utf-8")
 
-products = {} # Establish a dictionary that will hold each product Document. This dictionary is the Collection of those Documents.
+result = [] # This wrapper list will contain the dictionary that's the Collection of Documents.
 fields = ['ASIN', 'title', 'group', 'salesrank', 'similar']
 dict2 = {}
 current_ID = None # Establish a temporary variable to save the current ID of each product to make as the Document ID.
 document_count = 0
-document_limit = 200000
+document_limit = 100
 with open(filename, encoding="utf-8") as fp:
     for line in fp:
         description = list(line.strip().split(":", 1))
         if description[0] == "":
             if current_ID: # If the current ID is identified for the product.
-                products[f"{current_ID}"] = dict2
+                result.append(dict2) # Append the Document to the resulting list.
             dict2 = {}
             current_ID = None
             document_count += 1
 
             if document_count == document_limit:
-                break # This helps us limit the output data to 200,000 entries. We really don't need 1/2 million products here, haha.
+                break # This helps us limit the output data to the desired number of entries. We really don't need 1/2 million products here, haha.
         
         elif description[0] == 'Id':
             current_ID = description[1].strip() # It's necessary to set current_ID first so that we can know when to proceed to the next Document.
+            dict2["Id"] = current_ID
         elif description[0] == 'categories':
             num = int(description[1])
             cate = [description[1].strip()]
@@ -42,9 +43,7 @@ with open(filename, encoding="utf-8") as fp:
             if description[0] == field:
                 dict2[field] = description[1].strip()
 
-# Wrapping our dictonary of products with their ID in a Collection titled "products"
-final_dict = {"products": products}
-json.dump(final_dict, out, indent = 4, sort_keys = False)
+json.dump(result, out, indent = 4, sort_keys = False)
 out.close()
 
 # Full information about Amazon Share the Love products
