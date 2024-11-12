70a47e392cee689c23a76485b14e7d2afe58efed
Joshua Chadwick <jchadwick423@gmail.com>
2024-11-11 18:40:56 -0800

    Modified computations.py to extract data from a local MongoDB installation.

--- computations.py
+++ computations.py
@@ -1,9 +1,16 @@
-from pyspark.sql import SparkSession
+import pyspark
+from pyspark import SparkContext, SparkConf
+from pyspark.sql import SparkSession, SQLContext, functions as f
+from pyspark.sql.functions import *
 
-dbname = "amazon.json"
-spark = SparkSession.builder.appName("Amazon").getOrCreate()
-df = spark.read.json(dbname)
-#df.show()
+spark = SparkSession.builder.appName("AmazonRecommender") \
+                    .config("spark.jars.packages", "org.mongodb.spark:mongo-spark-connector_2.12:10.4.0") \
+                    .config("spark.mongodb.read.connection.uri", "mongodb://localhost:27017") \
+                    .config("spark.mongodb.read.database", "AmazonDB") \
+                    .config("spark.mongodb.read.collection", "Products") \
+                    .getOrCreate()
+df = spark.read.format("mongodb").load()
+df.show()
 
 def helper(line):
     #check if the current item is the item we are selecting to find similar items for
