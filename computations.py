68a883c43d6aa3ffa9f94290beb19a9107ccc400
Joshua Chadwick <jchadwick423@gmail.com>
2024-11-16 13:09:25 -0800

    Modified query matching algorithm to match with token substrings.

--- computations.py
+++ computations.py
@@ -41,20 +41,39 @@ def identifyRelated(line):
     #return the list
     return similarItems
 
-def queryMatchingItems(query):
+def queryMatchingItems(query, category=None):
     tokenized_query = query.split()
     cleansed_query = [re.sub(r'[^a-zA-Z0-9]', '', tok) for tok in tokenized_query] # Using a broad pattern to remove non alpha-numeric characters from the query
 
     # There is now the necessity to broadcast the tokens to all Spark nodes for efficient computation:
-    query_broadcast = f.array([f.lit(tok) for tok in cleansed_query]) # Broadcasting query tokens to the Spark nodes. (Since this project is local, there's only one node but whatevah.)
+    query_broadcast = spark.sparkContext.broadcast(cleansed_query) # Broadcasting query tokens to the Spark nodes. (Since this project is local, there's only one node but whatevah.)
     tokenized_df = df.withColumn("tokenized_title", f.split(f.col("Title"), "\\s+")) # Making a tokenized version of the dataframe
-    tokenized_df = tokenized_df.withColumn("matching_token_count", f.array_intersect(f.col("tokenized_title"), query_broadcast))
-    tokenized_df = tokenized_df.filter(f.size(f.col("matching_token_count")) > 2) # Setting some arbitrary value for token count. (I made it so that we need to find 2 or more related tokens to display the item)
-    tokenized_df = tokenized_df.withColumn("token_match_count", f.size(f.col("matching_token_count")))
-    tokenized_df = tokenized_df.orderBy(f.col("token_match_count").desc()) # Sort by descending order (So we can start with the highest number of matching tokens)
+    
+    # Exploding the tokenized titles so that we can process each token and identify matching substrings.
+    columns_to_preserve = [col for col in tokenized_df.columns if col != "tokenized_title"]
+    exploded_tokens = tokenized_df.select(*columns_to_preserve, f.explode(f.col("tokenized_title")).alias("token"))
+
+    # Filter out the tokens based on the given condition
+    conditions = [f.col("token").contains(f'{token}') for token in cleansed_query]
+    filter_condition = f.reduce(lambda x, y: x | y, conditions)
+    filtered_tokenized_df = exploded_tokens.filter(filter_condition)\
+
+    # Regroup the filtered dataframe    
+    filtered_tokenized_df = filtered_tokenized_df.groupBy("Title").agg(f.collect_set("token").alias("matching_tokens"))
+    
+    # Applying a new column with the matching tokens
+    filtered_tokenized_df = filtered_tokenized_df.withColumn("matching_tokens", f.array_intersect(f.col("tokenized_title"), f.array(*[f.lit(tok) for tok in query_broadcast.value])))
+    filtered_tokenized_df = filtered_tokenized_df.filter(f.size(f.col("matching_tokens")) > 2) # Setting some arbitrary value for token count. (I made it so that we need to find 2 or more related tokens to display the item)
+    
+    # If we specify a category, then we should filter based on the category too.
+    if category:
+        filtered_tokenized_df = filtered_tokenized_df.filter(f.col("category") == category)
+    
+    filtered_tokenized_df = filtered_tokenized_df.withColumn("token_match_count", f.size(f.col("matching_tokens")))
+    filtered_tokenized_df = filtered_tokenized_df.orderBy(f.col("token_match_count").desc()) # Sort by descending order (So we can start with the highest number of matching tokens)
 
     # TODO: Format the n number of matching items for the query and return the data in a way that's fitting for the application. (Or return nothing if a query fails to find matching items.)
-    return
+    return filtered_tokenized_df
 
 '''def findSimilarItems():
     df.printSchema()
